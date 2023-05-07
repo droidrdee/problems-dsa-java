83838559b0e4bb6e42765f3d937da3aa97b4ea40
Deepak Rathore <deepak.yourdomain@gmail.com>
2023-05-07 15:44:50 +0530

    leetcode 58

--- questions/lengthOfLastWord58.java
+++ questions/lengthOfLastWord58.java
@@ -1,2 +1,14 @@
-package questions;public class lengthOfLastWord58 {
+package questions;
+
+public class lengthOfLastWord58 {
+    public static void main(String[] args) {
+        String  s = "   fly me   to   the moon  ";
+        System.out.println(lengthOfLastWord(s));
+    }
+    static int lengthOfLastWord(String s) {
+        String[] words = s.split("\\s+");
+        int len = words.length;
+
+
+    }
 }
